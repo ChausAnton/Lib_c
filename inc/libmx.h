b2ca008b7cf7a400e85580842d06709c7f0e6274
Anton Chaus <anchaus@c1r3p4.khpi.ucode-connect.study>
2020-11-20 15:55:34 +0200

    some func

---
+++ inc/libmx.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef HEADER
+#define HEADER
+
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+int mx_strlen(const char *);
+
+void mx_printstr(const char *);
+
+void mx_printchar(char);
+
+
+#endif
---
+++ src/mx_printstr.c
@@ -0,0 +1,6 @@
+#include "../inc/libmx.h"
+
+void mx_printstr(const char *s) {
+     int len = mx_strlen(s);
+     write(1, s, len);
+}
